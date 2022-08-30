d57844b2423f34524b6e56b8056da0a05a270b1e
Stanslous <stancemuindi21@gmail.com>
2022-08-30 06:21:40 +0300

    Completed assingment

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
-}
+  let feet;
+  if ( distance < 400 ) {
+    feet = 'This one is on me!';
+  } if ( distance > 2000 ) {
+    feet = 'I will gladly take your thirty bucks.';
+  } if ( distance > 2500) { 
+    feet = 'No can do.';
+  }
+  return feet;
+};
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+scuberGreetingForFeet(190);
 
-function switchOnCharmFromTip(){
+function ternaryCheckCity( city ){
   // Write your code here!
+  return city === 'NYC'?'Ok, sounds good.':'No go.'
+};
+
+ternaryCheckCity('NYC');
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  switch(tip) {
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
 }
